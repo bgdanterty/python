8a73eeb7d52ff58c88a7cb319876ed6407cd4f27
bohdantertyshnyi <bgdanterty@gmail.com>
2022-06-28 22:23:12 +0300

    day 39

--- Flight Deal Finder/flight_search.py
+++ Flight Deal Finder/flight_search.py
@@ -7,6 +7,5 @@ TEQUILA_API_KEY = "Cr0FkjYNYeDBzsqDSJZHFGRrpAzeOnwY"
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
         code = "TESTING"
         return code
